27c1e54498645c218b98223393c99e429c09e50b
Digamber Singh <digamber@positronx.io>
2020-04-22 01:50:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# vue-image-preview
+[Vue.js Custom Image Preview example tutorial](https://www.positronx.io/vue-image-preview-tutorial/)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <filePreview />
+  </div>
+</template>
+
+<script>
+import filePreview from './components/filePreview.vue'
+
+export default {
+  name: 'App',
+  components: {
+    filePreview
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
